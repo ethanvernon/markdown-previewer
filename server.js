7839fcd43b186afae9d656b119e1b586723a98c1
Ethan Vernon <ethan.vernon@colorado.edu>
2019-04-20 10:06:34 +0800

    adds CRU methods to server.js

--- server.js
+++ server.js
@@ -29,34 +29,86 @@ let db = mongoose.connection;
 
 db.once("open", () => console.log("connected to the database"));
 
-// checks if connection with the database is successful
+//checks if connection with the database is successful
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-//post request from form in for new user Home.js
-// app.post("/api/new-metadata", function(req,res) {
+//post request to save user's markdown
+app.post("/api/new-file", function(req,res) {
 
-// 	var metadataToAdd = new Files({
-// 		name: req.body.name,
-// 		type: req.body.type,
-// 		size: req.body.size
-// 	});
+	let randomKey=generate()+generate()+generate();
 
-// 	metadataToAdd.save((err, response) => {
-// 		if (err) {
-// 			console.log("error to databse: " + err);
-// 			return res.json({success: false, error: err});
-// 		}
-// 		console.log("success, response is: " + response);
-// 		let condensedResponse = {
-// 			"name": response.name,
-// 			"type": response.type,
-// 			"size": response.size
-// 		};
-// 		return res.json(condensedResponse);
-// 	});
-// });
+	var fileToAdd = new Files({
+		user: req.body.user,
+		passkey: randomkey,
+		code: req.body.code
+	});
 
-//git request
+	fileToAdd.save((err, response) => {
+		if (err) {
+			console.log("error to databse: " + err);
+			return res.json({success: false, error: err});
+		}
+		console.log("success, response is: " + response);
+		let condensedResponse = {
+			"user": response.user,
+			"passkey": response.passkey,
+			"code": response.code
+		};
+		return res.json(condensedResponse);
+	});
+});
 
-// launch our backend into a port
+//get request for user's markdown
+app.get("/api/get-file", (req, res) => {
+
+	var user=req.query.user;
+	var passkey=req.query.passkey;
+
+	var query = Files.find();
+
+	query=query.where("user").equals(user);
+	query=query.where("passkey").equals(passkey);
+
+	query.exec((err, data) => {
+		if (err) throw err;	
+		//var dataResponse=JSON.parse(data);
+		res.json(data);
+	});
+});
+
+//this method overwrites existing data in our database
+app.post("/api/save-file", (req, res) => {
+	var passkey = req.query.passkey;
+	var update = req.query.update;
+
+	Files.findOneAndUpdate(passkey, update, err => {
+		if (err) return res.json({ success: false, error: err });
+		return res.json({ success: true });
+	});
+});
+
+//launch our backend into a port
 app.listen(API_PORT, () => console.log("LISTENING ON PORT ${API_PORT}"));
+
+//generates string of random letter a-z of string_length
+function generate_random_string(string_length){
+	let random_string = "";
+	let random_ascii;
+	for(let i = 0; i < string_length; i++) {
+		random_ascii = Math.floor((Math.random() * 25) + 97);
+		random_string += String.fromCharCode(random_ascii)
+	}
+	return random_string
+}
+
+//generates random number 1-9
+function generate_random_number(){
+	let num_low = 1;
+	let num_high = 9;
+	return Math.floor((Math.random() * (num_high - num_low)) + num_low);
+}
+
+//generates random string between 1-3 letters and random number (credit: https://codehandbook.org/generate-random-string-characters-in-javascript/ )
+function generate() {
+	return generate_random_string(Math.floor((Math.random()*3)+1)) + generate_random_number()
+}
