b693d506f67b77a306534ff71ca3daca46aec75b
Ethan Vernon <ethan.vernon@colorado.edu>
2019-04-25 14:56:27 +0800

    removes demo code from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,30 +10,13 @@ import OutputBox from './OutputBox';
 import { Container, Row } from 'reactstrap';
 
 class App extends Component {
- constructor(props) {
-   super(props);
+	constructor(props) {
+		super(props);
 
-   this.state = {
-     input: ''
-   }
-
-   this.handleChange = this.handleChange.bind(this);
-   this.submitMessage = this.submitMessage.bind(this);
- }
-
- handleChange(event) {
-   this.setState({
-     input: event.target.value
-   });
- }
-
- submitMessage() {
-   this.props.submitNewMessage(this.state.input);
-
-   this.setState({
-     input: ''
-   });
- }
+		this.state = {
+			input: ''
+		}
+	}
 
 	render() {
 		return (
@@ -46,41 +29,9 @@ class App extends Component {
 						<OutputBox />
 					</Row>
 				</Container>
-
-
-				<h2>Type in a new Message:</h2>
-				<input
-				value={this.state.input}
-				onChange={this.handleChange}/><br/>
-				<button onClick={this.submitMessage}>Submit</button>
-				<ul>{
-				this.props.messages.map( (message, idx) => {
-				return (
-				<li key={idx}>{message}</li>
-				)
-				})
-				}
-				</ul>
 			</div>
 		);
 	}
 };
 
-const mapStateToProps = ({ messages }) => {
-  /**
-   * Destructuring out messages from state and assigning it to the key
-   * messages, since the key and value are bother the same you can leave out
-   * messages: messages and just have messages, both are the same
-   */
-  return { messages }
-};
-
-const mapDispatchToProps = (dispatch) => {
- return {
-   submitNewMessage: (message) => {
-     dispatch(addMessage(message))
-   }
- }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(null, null)(App);
